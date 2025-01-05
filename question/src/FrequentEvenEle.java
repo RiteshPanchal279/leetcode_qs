dca2af4436ad040e9831d4959a10ba8a5bb1adca
RiteshPanchal279 <riteshpanchal279@gmail.com>
2025-01-05 14:48:06 +0530

    fouth

--- question/src/FrequentEvenEle.java
+++ question/src/FrequentEvenEle.java
@@ -1,2 +1,32 @@
-package PACKAGE_NAME;public class FrequentEvenEle {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class FrequentEvenEle {
+    public static void main(String[] args) {
+        int[] arr= {2,2,3,4};
+        int ans=evenFreqElement(arr);
+        System.out.println(ans);
+    }
+
+    static int evenFreqElement(int[] arr){
+//        note: in HashMap no duplication "key" allowed
+        Map<Integer, Integer> map = new HashMap<>();
+        int maxFreq = 0, res = -1, curFre = 0;
+        for(int val : arr){
+            if(val % 2 == 1) continue;//skip the odd number
+//    curFre will contain the current frequency of one even number
+//    incrementing with +1 if some even number comes again and that is current frequency
+//            eg: curFre = 0 + 1
+            curFre = map.getOrDefault(val, 0) + 1;
+//            now putting key,value as eg:2=1 next 2=2,3=1,4=1
+            map.put(val, curFre);
+            if(curFre > maxFreq || curFre == maxFreq && val < res){
+//               assign the map key,value to other variable
+                maxFreq = curFre;
+                res = val;
+            }
+        }
+        return res;
+    }
 }
--- question/src/MajorityElement.java
+++ question/src/MajorityElement.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class MajorityElement {
+import java.util.Arrays;
+
+public class MajorityElement {
+    public static void main(String[] args) {
+       int[] nums = {5,3,7,9,0,1,1};
+       int and=majEle(nums);
+       Arrays.sort(nums);
+        System.out.println(and);
+        for(int a:nums){
+            System.out.print(a+",");
+        }
+    }
+
+    public static int majEle(int[] arr){
+        Arrays.sort(arr);
+        int n=arr.length;
+        return arr[n/2];
+    }
 }
